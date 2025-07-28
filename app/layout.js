785a072a251e9d9da317ef20aeb59d48fad04b18
yomarsh5858 <136950005+yomarsh5858@users.noreply.github.com>
2025-07-29 00:15:00 +0100

    navbar done

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head> 
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;700&family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        </head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
