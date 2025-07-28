09dc60c78586ff8e97592d615524442cc56462f0
yomarsh5858 <136950005+yomarsh5858@users.noreply.github.com>
2025-07-28 13:31:42 +0100

    BUILDING MY PORTFOLIO

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+export default function Navbar() {
+    return(
+        
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <ul className="flex space-x-4">hello</ul>
+            </div>
+        </nav>
+    )
+    }
